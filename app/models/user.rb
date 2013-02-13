ce0e372f8fdb65bdd6537a1efd183c48c2a61442
mohammedhuzaifa <zeefu@toobler.com>
2013-02-14 00:18:04 +0530

    Added tests for user first name,last name and profile correctness

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true, uniqueness: true, format: {with: /a-zA-Z0-9_-/,message: 'Must be formatted correctly'}
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
   def full_name
