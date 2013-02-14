c66911b2511b25c6cb6bcf3b0f4fe932edf7ba07
mohammedhuzaifa <zeefu@toobler.com>
2013-02-14 10:55:53 +0530

    Added few test cases for profile names

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :profile_name, presence: true, uniqueness: true, format: {with: /a-zA-Z0-9_-/,message: 'Must be formatted correctly'}
+  has_many :statuses
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
